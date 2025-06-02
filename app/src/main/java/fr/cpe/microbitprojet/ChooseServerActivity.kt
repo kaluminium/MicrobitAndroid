6981fe7e4e3d0b9618bebaa9cc8e53566da13e0e
kaluminium <lukalindberg@gmail.com>
2025-06-02 22:51:24 +0200

    feat(chooseServer): cache for server added

--- app/src/main/java/fr/cpe/microbitprojet/ChooseServerActivity.kt
+++ app/src/main/java/fr/cpe/microbitprojet/ChooseServerActivity.kt
@@ -1,6 +1,8 @@
 package fr.cpe.microbitprojet
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -12,14 +14,21 @@ import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.SocketTimeoutException
 import kotlin.concurrent.thread
+import androidx.core.content.edit
 
 class ChooseServerActivity : AppCompatActivity() {
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         actionBar?.hide()
         setContentView(R.layout.choose_server)
+
+        sharedPref = getSharedPreferences("myCache", Context.MODE_PRIVATE)
+
+        findViewById<EditText>(R.id.serveradress).setText(sharedPref.getString("server_adress", ""))
+        findViewById<EditText>(R.id.serverport).setText(sharedPref.getString("server_port", ""))
 
         findViewById<Button>(R.id.connect).setOnClickListener {
             val IP = findViewById<EditText>(R.id.serveradress).text.toString() // l'IP du serveur passerel
@@ -43,7 +52,11 @@ class ChooseServerActivity : AppCompatActivity() {
 
         UdpManager.getInstance().connect(serverAddress, serverPort, {
             runOnUiThread { connIsSuccess() }
-        }, {
+            sharedPref.edit {
+                putString("server_adress", serverAddress)
+                putString("server_port", serverPort.toString())
+                }
+            }, {
             runOnUiThread { connHasFailed() }
         })
     }
